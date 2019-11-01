9fadc05fe02332b443486c6844924e6899dc1b04
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-01 14:11:02 -0600

    completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function getDogImage(num) {
-  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+function getDogImage(breed) {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
     .then(response => response.json())
     .then(responseJson => {
       displayResults(responseJson);
@@ -11,20 +11,57 @@ function getDogImage(num) {
 
 
 
+
+
+//return a single random dog breed image
 function displayResults(responseJson) {
   console.log(responseJson);
   //replace the existing image with the new one
   $('.results-img').replaceWith(
     `<img src="${responseJson.message}" class="results-img">`
-  );
+  )
   //display the results section
   $('.results').removeClass('hidden');
 }
 
 
-// function pushUserRequest() {
-//   document.getElementById('userFillable').value = 
+
+
+
+//return 1-50 random images
+// function displayResults(responseJson) {
+  
+//   console.log(responseJson);
+//   for (let i = 0; i < responseJson.message.length; i++) { 
+//     $('.results').append(`<img src="${responseJson.message[i]}" class="results-img">`); 
+//     $('.results').removeClass('hidden');
+//   }
+
+// console.log(responseJson.message);
+// //display the results section
+// $('.results').removeClass('hidden');
 // }
+
+
+
+
+//   Create an app that loads a single random image for a specific breed, based on a user input. 
+//   This app should account for the happy case when the breed is found, as well as the unhappy case when it is not. 
+//   Use the endpoint described in the "RANDOM IMAGE FROM A BREED COLLECTION" section of this page of the DogAPI docs. 
+//   Note that the API will return an HTTP status code of 404 along with a JSON object with info 
+//   about the error if a request is made for a breed that can't be found.
+
+
+
+
+
+
+
+
+
+
+
+
 
 function watchForm() {
   $('form').submit(event => {
