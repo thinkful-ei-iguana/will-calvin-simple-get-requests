7f12ee547c35a9e7b9cf7aa908ce5b2dcbe4359d
Jekel8888 <52115322+Jekel8888@users.noreply.github.com>
2019-11-01 10:45:33 -0600

    just before lunch, partial to adding images to DOM

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-// Create an app that lets users choose to display between 1 and 50 random dog images,
-//  then prints the results to the console. The app should feature a form with a required
-//   input where users indicate the number of images to retrieve, and the input should
-//    default to 3. Use the endpoint described in the "DISPLAY MULTIPLE RANDOM IMAGES FROM ALL DOGS COLLECTION" 
-//    section of this page of the DogAPI docs.
-
-
-
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImage(num) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => {
+      displayResults(responseJson);
+      console.log(responseJson);
+    });    
 }
+
+
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  );
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+
+// function pushUserRequest() {
+//   document.getElementById('userFillable').value = 
+// }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    let userNumber = $('#userFillable').val()
+    getDogImage(userNumber);
   });
 }
 
